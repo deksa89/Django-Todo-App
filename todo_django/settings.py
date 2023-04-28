4ef815507d0c4b9519d30be0ae7562e2324f40f1
Dean Hudek <hudek.dean@gmail.com>
2023-04-28 16:08:22 +0200

    database connected to psql

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -78,12 +78,23 @@ WSGI_APPLICATION = 'todo_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_app_db',
+        'USER': 'dean',
+        'PASSWORD': config("PASSWORD"),
+
     }
 }
+
 
 
 # Password validation
